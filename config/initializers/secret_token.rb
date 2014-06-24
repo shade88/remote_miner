d4580902d5a0dacc93080e2927728c5adc997281
shade88 <shade88@yandex.ru>
2014-06-24 16:27:02 +0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RemoteMiner::Application.config.secret_key_base = 'b5af79a1fd358e3f29f48e05cc040c54023e44595fcc0c82c472c12cfc2a0de191b1626b5c025cc23a75e3b25154522438ee4e6f150b3b392d69ad32c23afb50'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RemoteMiner::Application.config.session_store :cookie_store, key: '_remote_miner_session'
